2a054adf29483d7fb25a27513e62d479208076f0
HEHEBOY78 <80183270+HEHEBOY78@users.noreply.github.com>
2026-05-13 15:16:33 +0300

    Create ManhwaSusu.kt

---
+++ src/en/manhwasusu/src/eu/kanade/tachiyomi/extension/en/manhwasusu/ManhwaSusu.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.en.manhwasusu
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class ManhwaSusu : Madara(
+    "ManhwaSusu",
+    "https://manhwasusu.com",
+    "en",
+)
