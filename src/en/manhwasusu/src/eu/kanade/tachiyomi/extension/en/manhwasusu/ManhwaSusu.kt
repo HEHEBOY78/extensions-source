d3ee4282fdca369061735296e286bb7632981d31
HEHEBOY78 <80183270+HEHEBOY78@users.noreply.github.com>
2026-05-13 21:28:08 +0300

    Update ManhwaSusu.kt

--- src/en/manhwasusu/src/eu/kanade/tachiyomi/extension/en/manhwasusu/ManhwaSusu.kt
+++ src/en/manhwasusu/src/eu/kanade/tachiyomi/extension/en/manhwasusu/ManhwaSusu.kt
@@ -1,12 +1,9 @@
 package eu.kanade.tachiyomi.extension.en.manhwasusu
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.multisrc.madara.MadaraUrlActivity
 
 class ManhwaSusu : Madara(
     "ManhwaSusu",
     "https://manhwasusu.com",
     "en",
 )
-
-class ManhwaSusuUrlActivity : MadaraUrlActivity()
